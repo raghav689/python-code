1440a5058daec02192415dcdfa8d103f37969025
kiranrajawat <rajawatkiran007@gmail.com>
2024-09-03 14:11:25 +0530

    added condition_statement

---
+++ condition_statement.py
@@ -0,0 +1,12 @@
+a = int(input("ENTER YOUR DIGIT HERE :" ))
+if a % 2 == 0 :
+    print("It is an even number")
+else:
+    print("It is an odd number")
+
+"""
+    answer:-
+    ENTER YOUR DIGIT HERE :6465702
+    It is an even number
+                        """
+
