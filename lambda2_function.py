ae720ed0fb62824c75ac2b1dc2cbddb254732b0e
kiranrajawat <rajawatkiran007@gmail.com>
2024-09-04 16:13:14 +0530

    added lambda2_function

---
+++ lambda2_function.py
@@ -0,0 +1,6 @@
+city = [ 'jaipur','jodhpur','dausa','bikaner','sikar','jaisalmer','alwar','ajmer']
+sort = sorted(city, key=lambda x: len(x))
+print("sorted words by length :", sort)
+
+   # answer :-
+ # sorted words by length : ['dausa', 'sikar', 'alwar', 'ajmer', 'jaipur', 'jodhpur', 'bikaner', 'jaisalmer']
